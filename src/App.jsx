7623e032818246eab3b7a85d79495dd9455885ea
mishadiatel <mishadiatel13@gmail.com>
2023-05-28 13:55:23 +0300

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import {useEffect} from "react";
 import {fetchDataFromApi} from "./utils/api.js";
 import {useDispatch, useSelector} from "react-redux";
 import {getApiConfiguration} from "./store/homeSlice.js";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import Details from "./pages/details/Details.jsx";
+import SearchResult from "./pages/searchResult/SearchResult.jsx";
+import Explore from "./pages/explore/Explore.jsx";
+import PageNotFound from "./pages/404/PageNotFound.jsx";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
 
 
 function App() {
@@ -21,10 +29,17 @@ function App() {
    }
 
     return (
-        <div className={'App'}>
-            App <br/>
-            {url?.total_pages}
-        </div>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path={'/'} element={<Home />} />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
     )
 }
 
