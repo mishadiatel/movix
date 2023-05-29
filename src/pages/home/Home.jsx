e3eaff1f95b45cccdf687260f889f4fa5adbe4fb
mishadiatel <mishadiatel13@gmail.com>
2023-05-29 14:14:01 +0300

    added trending films with carousel with posters

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './style.scss'
 import HeroBanner from "./heroBanner/HeroBanner.jsx";
+import Trending from "./trending/Trending.jsx";
 const Home = () => {
     return (
         <div className={'homePage'}>
             <HeroBanner />
+            <Trending />
         </div>
     );
 };
