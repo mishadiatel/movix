91a0f2fe02cb49ba9c0720c219e577787e4d82ce
mishadiatel <mishadiatel13@gmail.com>
2023-05-30 14:44:21 +0300

    added details page

---
+++ src/components/videoPopup/VideoPopup.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactPlayer from "react-player/youtube";
+
+import "./style.scss";
+import PropTypes from "prop-types";
+
+const VideoPopup = ({ show, setShow, videoId, setVideoId }) => {
+    const hidePopup = () => {
+        setShow(false);
+        setVideoId(null);
+    };
+    return (
+        <div className={`videoPopup ${show ? "visible" : ""}`}>
+            <div className="opacityLayer" onClick={hidePopup}></div>
+            <div className="videoPlayer">
+                <span className="closeBtn" onClick={hidePopup}>
+                    Close
+                </span>
+                <ReactPlayer
+                    url={`https://www.youtube.com/watch?v=${videoId}`}
+                    controls
+                    width="100%"
+                    height="100%"
+                    // playing={true}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default VideoPopup;
+VideoPopup.propTypes = {
+    show: PropTypes.bool,
+    setShow: PropTypes.func,
+    videoId: PropTypes.string,
+    setVideoId: PropTypes.func
+}
---
+++ src/pages/details/carousels/Similar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Carousel from "../../../components/carousel/Carousel";
+import useFetch from "../../../hooks/useFetch";
+import PropTypes from "prop-types";
+
+const Similar = ({ mediaType, id }) => {
+    const { data, loading, error } = useFetch(`/${mediaType}/${id}/similar`);
+
+    const title = mediaType === "tv" ? "Similar TV Shows" : "Similar Movies";
+
+    return (
+        <Carousel
+            title={title}
+            data={data?.results}
+            loading={loading}
+            endpoint={mediaType}
+        />
+    );
+};
+
+export default Similar;
+
+Similar.propTypes = {
+    mediaType: PropTypes.string,
+    id: PropTypes.string
+}
