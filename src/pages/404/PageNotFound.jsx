4c11c4a12899e2aafedaf6c21e55ba08b3197a94
mishadiatel <mishadiatel13@gmail.com>
2023-05-31 12:08:26 +0300

    added explore page and PageNotFound

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import './style.scss'
+import React from "react";
+
+import "./style.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
 const PageNotFound = () => {
     return (
-        <div>
-            PageNotFound
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
         </div>
     );
 };
